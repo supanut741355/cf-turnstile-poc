592cda223f7593f1cb0f93a8b924ca347eddce0e
supanut741355 <supanut.wizard@gmail.com>
2026-09-19 15:15:42 +0700

    set up fe project

---
+++ cf-turnstile-fe/src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+      Tailwind CSS is working!
+      </h1>
+    </>
+  )
+}
+
+export default App
